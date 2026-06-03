dbaa0d3fb560e34565d7dee4847b53790ec1b428
geoffreyFRANZ <61542992+GeoffreyFRANZ@users.noreply.github.com>
2026-06-03 14:57:21 +0200

    serialize concurrent RevertColor calls with a mutex

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -5,15 +5,21 @@ package engine
 #include "engine.h"
 */
 import "C"
-import "unsafe"
+import (
+	"sync"
+	"unsafe"
+)
 
 type Engine struct {
 	eng *C.opencl_engine
+	mu  sync.Mutex
 }
 
 func New() *Engine {
-	return &Engine{C.create_engine()}
+	return &Engine{eng: C.create_engine()}
 }
 func (e *Engine) RevertColor(pixels unsafe.Pointer, size int) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
 	C.revert_color(e.eng, (*C.uchar)(pixels), C.int(size))
 }
